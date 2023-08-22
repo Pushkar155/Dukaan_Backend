3306da59f276be58d80514c344a87c767f88c88f
Pushkar155 <96383324+Pushkar155@users.noreply.github.com>
2023-08-22 11:48:19 +0530

    adding user auth

---
+++ router/login.js
@@ -0,0 +1,41 @@
+const router=require("express").Router();
+
+const LoginSchema=require("../schema/Loginschema");
+
+
+router.post('/login', async (req,res)=>{
+
+    const{name,password}=req.body;
+    try {
+        let exist = await LoginSchema.findOne({name});
+        if(!exist){
+            const newuser= new LoginSchema({
+                name,
+                password
+            });
+            await newuser.save();
+            res.json({message:"New User Updated"});
+        }
+        
+    } catch (error) {
+        return res.status(500).send("Server Error")
+    }
+})
+
+router.get('/login', async (req,res)=>{
+    const{name,password}=req.body;
+    try {
+        let exist = await LoginSchema.findOne({name});
+        if(exist){
+            if(exist.password===password){
+                res.status(201).json({"message":"success full login"});
+            }
+            if(exist.password!=password){
+                return  res.status(403).send('wrong password');
+            }
+        }
+        
+    } catch (error) {
+        return res.status(403).send("Wrong Credentials")
+    }
+})
