242d53982b2f004ce822035d92a5dda76cc3e670
Pushkar155 <96383324+Pushkar155@users.noreply.github.com>
2023-08-24 18:46:41 +0530

    fix : all isuee

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,17 +2,20 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
     const token = req.header('Authorization');
+    const TokenArray = token.split(" ");
+    // console.log(TokenArray)
   
     if (!token) {
       return res.status(401).json({ message: 'Access denied by token' });
     }
   
     try {
-      const decodedToken = jwt.verify(token, process.env.SECRETEKEY);
+      const decodedToken = jwt.verify(TokenArray[1], process.env.SECRETEKEY);
       req.userId = decodedToken.userId;
+    //   console.log(req.userId,decodedToken.userId)
       next();
     } catch (error) {
-      res.status(400).json({ message: 'Invalid token' });
+      res.status(400).json({ message: `Invalid token`});
     }
   };
 module.exports=verifyToken;
